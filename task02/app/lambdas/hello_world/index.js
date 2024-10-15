d14e699c6ceaff73235cc0dd823e97afd98c714f
Davit Karseladze <davit_karseladze@epam.com>
2024-10-16 03:10:03 +0400

    update index

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,16 +1,19 @@
 exports.handler = async (event) => {
-    const path = event.rawPath || '/';
-    const method = event.requestContext ? event.requestContext.http.method : 'GET';
+    const { httpMethod, path } = event;
 
-    if (path === '/hello' && method === 'GET') {
+    if (path === "/hello" && httpMethod === "GET") {
         return {
             statusCode: 200,
-            message: "Hello from Lambda"
+            body: JSON.stringify({
+                message: "Hello from Lambda"
+            })
         };
     }
-
+    
     return {
         statusCode: 400,
-        message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
+        body: JSON.stringify({
+            message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${httpMethod}`
+        })
     };
 };
